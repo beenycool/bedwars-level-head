45f083a55a78ad80ad69efc4a846d5e68b26d0e3
beenycool <129210955+beenycool@users.noreply.github.com>
2026-04-12 11:18:09 +0100

    Delete test3.js

--- test3.js
+++
@@ -1,7 +0,0 @@
-const filters = {
-  from: "2023-01-01T00:00:00.000Z",
-  to: "2023-12-31T23:59:59.999Z",
-  limit: 100
-};
-const json = JSON.stringify({ filters }).replace(/</g, '\\u003c');
-console.log(json);
