3c354e7b89296ab703d3219569d94c35f6db9595
beenycool <129210955+beenycool@users.noreply.github.com>
2026-04-17 16:44:02 +0000

    Apply reviewer suggestion: use preset.displayName in hover text

--- src/main/kotlin/club/sk1er/mods/levelhead/commands/LevelheadCommand.kt
+++ src/main/kotlin/club/sk1er/mods/levelhead/commands/LevelheadCommand.kt
@@ -657,7 +657,7 @@ class LevelheadCommand {
                             preset.displayName,
                             run = false,
                             suggestedCommand = "/levelhead profile apply ${preset.name}",
-                            hoverTextOverride = "${ChatColor.GREEN}Click to prepare to apply ${preset.name}"
+                            hoverTextOverride = "${ChatColor.GREEN}Click to prepare to apply ${preset.displayName}"
                         )
                     ).appendSibling(ChatComponentText("${ChatColor.YELLOW}: ${ChatColor.GRAY}${preset.description}"))
                     sendMessage(line)
